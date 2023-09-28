016c0547bbf2bc1d9c147886f624a25792e57220
Provoski <provo.iyen27@gmail.com>
2023-09-28 23:56:04 +0000

    created 4-pricing.js

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,42 +1,45 @@
-class Currency {
-  constructor(code, name) {
-    // Store attributes with underscores
-    this._code = code;
-    this._name = name;
+const Currency = require('./3-currency');
+
+class Pricing {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
   }
 
-  // Getter for code
-  get code() {
-    return this._code;
+  get amount() {
+    return this._amount;
   }
 
-  // Setter for code
-  set code(newCode) {
-    if (typeof newCode === 'string') {
-      this._code = newCode;
+  set amount(newAmount) {
+    if (typeof newAmount === 'number') {
+      this._amount = newAmount;
     } else {
-      throw new Error('Code must be a string');
+      throw new Error('Amount must be a number');
     }
   }
 
-  // Getter for name
-  get name() {
-    return this._name;
+  get currency() {
+    return this._currency;
   }
 
-  // Setter for name
-  set name(newName) {
-    if (typeof newName === 'string') {
-      this._name = newName;
+  set currency(newCurrency) {
+    if (newCurrency instanceof Currency) {
+      this._currency = newCurrency;
     } else {
-      throw new Error('Name must be a string');
+      throw new Error('Currency must be an instance of Currency class');
     }
   }
 
-  // Method to display full currency
-  displayFullCurrency() {
-    return `${this._name} (${this._code})`;
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    if (typeof amount === 'number' && typeof conversionRate === 'number') {
+      return amount * conversionRate;
+    }
+    throw new Error('Both amount and conversionRate must be numbers');
   }
 }
 
-export default 
+export default Pricing;
