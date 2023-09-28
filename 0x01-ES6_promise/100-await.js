14d97d016b718dc2497cb7b58bb2781666c941e7
Provoski <provo.iyen27@gmail.com>
2023-09-28 02:58:49 +0000

    created 100-await.js

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,20 @@
+import { uploadPhoto, createUser } from './utils';
+
+async function asyncUploadUser() {
+  try {
+    const [photo, user] = await Promise.all([uploadPhoto(), createUser()]);
+
+    return {
+      photo,
+      user,
+    };
+  } catch (error) {
+    console.error('An error occurred:', error);
+    return {
+      photo: null,
+      user: null,
+    };
+  }
+}
+
+export default asyncUploadUser;
