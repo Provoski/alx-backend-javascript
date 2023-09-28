86ceacb188b7359b64c36c236e5852752f734240
Provoski <provo.iyen27@gmail.com>
2023-09-28 00:51:08 +0000

    created project directory, reademe file and 0-promise.js file

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,20 @@
+export function getResponseFromAPI() {
+  return new Promise((resolve, reject) => {
+    // Simulate an API call or asynchronous operation
+    setTimeout(() => {
+      // Replace this with the actual data you want to resolve
+      const responseData = { message: "API response data" };
+
+      // Check if the operation was successful or encountered an error
+      const success = true; // Set to true for success, false for error (for demonstration)
+
+      if (success) {
+        // Resolve the Promise with the data
+        resolve(responseData);
+      } else {
+        // Reject the Promise with an error message
+        reject("API request failed");
+      }
+    }, 1000); // Simulating a delay of 1 second
+  });
+}
---
+++ 0x01-ES6_promise/README.md
@@ -0,0 +1,1 @@
+directiry for promise task
