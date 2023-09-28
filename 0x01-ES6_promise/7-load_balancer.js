620706e4bdb254609485af897af028efc6cebb51
Provoski <provo.iyen27@gmail.com>
2023-09-28 02:38:12 +0000

    created 7-load_balancer.js

---
+++ 0x01-ES6_promise/7-load_balancer.js
@@ -0,0 +1,6 @@
+function loadBalancer(chinaDownload, USDownload) {
+  // Use Promise.race to resolve with the value of the first resolved promise
+  return Promise.race([chinaDownload, USDownload]);
+}
+
+export default loadBalancer;
