f978f36cd335881bfb7712dcf1b129984d79e34e
Provoski <provo.iyen27@gmail.com>
2023-09-28 01:13:09 +0000

    updated 0-promise.js

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,9 +1,9 @@
-export function getResponseFromAPI() {
+function getResponseFromAPI() {
   return new Promise((resolve, reject) => {
     // Simulate an API call or asynchronous operation
     setTimeout(() => {
       // Replace this with the actual data you want to resolve
-      const responseData = { message: "API response data" };
+      const responseData = { message: 'API response data' };
 
       // Check if the operation was successful or encountered an error
       const success = true; // Set to true for success, false for error (for demonstration)
@@ -13,8 +13,9 @@ export function getResponseFromAPI() {
         resolve(responseData);
       } else {
         // Reject the Promise with an error message
-        reject("API request failed");
+        reject(new Error('API request failed'));
       }
     }, 1000); // Simulating a delay of 1 second
   });
 }
+export default getResponseFromAPI;
